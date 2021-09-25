d68dc04e5884b0f0b2ccdf040f79afdc8a6a4d34
Isaa Jose Bueno <ibueno.dev@gmail.com>
2021-09-25 18:03:39 -0300

    Projeto criado

---
+++ src/test/java/com/ibueno21/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibueno21.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
